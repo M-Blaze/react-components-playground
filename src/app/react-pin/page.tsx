dace9c42544731c6afbde03e706d5b18817120e8
Moulik Rai <raimoulik@gmail.com>
2023-05-28 07:58:56 +0545

    refactor: use CopyToClipBoard component

--- src/app/react-pin/page.tsx
+++ src/app/react-pin/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import CopyToClipboard from "../components/CopyToClipboard"
 import ReactPin from "./ReactPin"
 
 export default function ReactPinPage() {
@@ -7,6 +8,7 @@ export default function ReactPinPage() {
       <div className="block-header text-center">
         <div className="container">
           <h1>React Pin</h1>
+          <p>Use <CopyToClipboard text='npm -i react-pin-widget'/> or <CopyToClipboard text='yarn add react-pin-widget'/> to add this component in your project</p>
         </div>
       </div>
       <div className="component-wrapper text-center">
