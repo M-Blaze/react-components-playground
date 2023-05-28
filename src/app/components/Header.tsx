408607c93cafcd06f52c5b89e66be0d1a96d74c3
Moulik Rai <raimoulik@gmail.com>
2023-05-28 17:53:46 +0545

    chore: add header component

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <div className="header-content flex justify-between items-center" style={{ padding: '10px 0', background: 'white' }}>
+          <div className="logo">
+            <Link href={'/'}>
+              <Image src='/logo.png' width={220} height={50} alt='React Component Playground' priority />
+            </Link>
+          </div>
+          <nav>
+            <ul className="nav-list">
+              <li>
+                <a href="https://moulik.vercel.app/" target='_blank'>About me</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
