4df84f9fac874e6dddcea86cd80d9293c6a81d44
Moulik Rai <raimoulik@gmail.com>
2023-07-22 11:05:46 +0545

    chore: add vercel analytics in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 
 import Header from './components/Header'
 
@@ -18,6 +19,7 @@ export default function RootLayout({
           {children}
         </main>
       </body>
+      <Analytics />
     </html>
   )
 }
